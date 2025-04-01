d3a4d2b485b394f8dbaa6888271c5767754ffc3e
Jitendra <jitendra@webkul.in>
2025-04-01 12:17:29 +0530

    Fixed installation issues

--- plugins/webkul/security/database/migrations/2024_12_10_101127_add_default_company_id_column_to_users_table.php
+++ plugins/webkul/security/database/migrations/2024_12_10_101127_add_default_company_id_column_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('default_company_id')
-                ->nullable(false)
+                ->nullable()
                 ->constrained('companies')
                 ->restrictOnDelete();
         });
