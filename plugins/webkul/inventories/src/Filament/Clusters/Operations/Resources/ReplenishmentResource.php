636f6614cd4959d595680bd4d5749a91e144bb4c
Jitendra <jitendra@webkul.in>
2025-04-03 18:17:32 +0530

    Issue #280 fixed (Not implemented yet)

--- plugins/webkul/inventories/src/Filament/Clusters/Operations/Resources/ReplenishmentResource.php
+++ plugins/webkul/inventories/src/Filament/Clusters/Operations/Resources/ReplenishmentResource.php
@@ -20,6 +20,9 @@ class ReplenishmentResource extends Resource
 
     protected static ?int $navigationSort = 4;
 
+    //Todo: Remove this when completed
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static ?string $cluster = Operations::class;
 
     protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
