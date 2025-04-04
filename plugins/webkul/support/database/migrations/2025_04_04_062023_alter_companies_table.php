caf281fb07e4029a64f9dab32046b9d3be213ba9
Jitendra <jitendra@webkul.in>
2025-04-04 12:18:00 +0530

    Fixed duplicate column issue in migration

--- plugins/webkul/support/database/migrations/2025_04_04_062023_alter_companies_table.php
+++ plugins/webkul/support/database/migrations/2025_04_04_062023_alter_companies_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('companies', function (Blueprint $table) {
-            $table->string('partner_id')->nullable()->change();
             $table->string('company_id')->nullable()->change();
         });
     }
