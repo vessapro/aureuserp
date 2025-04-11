0266330d52da0dd47e78ab08f314be90e71a1efb
Jitendra <jitendra@webkul.in>
2025-04-11 16:55:21 +0530

    Issue #379 fixed

--- plugins/webkul/inventories/src/Models/OperationType.php
+++ plugins/webkul/inventories/src/Models/OperationType.php
@@ -110,12 +110,12 @@ class OperationType extends Model implements Sortable
 
     public function sourceLocation(): BelongsTo
     {
-        return $this->belongsTo(Location::class);
+        return $this->belongsTo(Location::class)->withTrashed();
     }
 
     public function destinationLocation(): BelongsTo
     {
-        return $this->belongsTo(Location::class);
+        return $this->belongsTo(Location::class)->withTrashed();
     }
 
     public function warehouse(): BelongsTo
