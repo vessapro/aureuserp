d890cb71bb99c10521df727b8c8b755a1a95c3c5
Jitendra <jitendra@webkul.in>
2025-04-17 13:27:25 +0530

    Issue #415 fixed

--- plugins/webkul/accounts/src/Filament/Resources/TaxResource/Pages/ViewTax.php
+++ plugins/webkul/accounts/src/Filament/Resources/TaxResource/Pages/ViewTax.php
@@ -4,9 +4,11 @@ namespace Webkul\Account\Filament\Resources\TaxResource\Pages;
 
 use Filament\Actions;
 use Filament\Notifications\Notification;
+use Illuminate\Database\QueryException;
 use Filament\Pages\SubNavigationPosition;
 use Filament\Resources\Pages\ViewRecord;
 use Webkul\Account\Filament\Resources\TaxResource;
+use Webkul\Account\Models\Tax;
 
 class ViewTax extends ViewRecord
 {
@@ -22,6 +24,17 @@ class ViewTax extends ViewRecord
         return [
             Actions\EditAction::make(),
             Actions\DeleteAction::make()
+                ->action(function (Tax $record) {
+                    try {
+                        $record->delete();
+                    } catch (QueryException $e) {
+                        Notification::make()
+                            ->danger()
+                            ->title(__('accounts::filament/resources/tax/pages/view-tax.header-actions.delete.notification.error.title'))
+                            ->body(__('accounts::filament/resources/tax/pages/view-tax.header-actions.delete.notification.error.body'))
+                            ->send();
+                    }
+                })
                 ->successNotification(
                     Notification::make()
                         ->success()
